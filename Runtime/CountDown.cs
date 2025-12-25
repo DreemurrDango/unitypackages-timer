fe39a2fdf623f4235b270ff130b5c2fd03f7011d
DreemurrDango <3011090456@qq.com>
2025-12-25 11:28:48 +0800

    已有内容更新

---
+++ Runtime/CountDown.cs
@@ -0,0 +1,158 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+namespace DreemurrStudio.CountDownTimer
+{
+
+    /// <summary>
+    /// 通用倒计时组件
+    /// </summary>
+    public class CountDown : MonoBehaviour
+    {
+        [Header("倒计时设置")]
+        [SerializeField]
+        [Tooltip("是否使用时间缩放")]
+        private bool useTimeScale = true;
+        [SerializeField]
+        [Tooltip("是否在按下任意键时重置倒计时")]
+        private bool resetOnAnyInput = true;
+        [Tooltip("倒计时结束时事件，输出倒计时运行的时间")]
+        public UnityEvent<float> onCountDownCompleted;
+
+        [Header("文本显示")]
+        [SerializeField]
+        [Tooltip("显示倒计时的文本组件")]
+        private TMP_Text timeValueText;
+        [SerializeField]
+        [Tooltip("显示倒计时的文本内容模式，可添加的参数包括{0:剩余时间}{1:完整时间}{2:已经过时间}{3:剩余时间百分比}")]
+        private string contentFormat = "{0}";
+
+        [Header("进度条显示")]
+        [SerializeField]
+        [Tooltip("显示倒计时进度的滑动条")]
+        private Slider timeValueSlider;
+
+        [Header("调试")]
+        [SerializeField]
+        [Tooltip("倒计时的完整时间")]
+        private float countDownFullTime;
+        [SerializeField]
+        [Tooltip("剩余倒计时时间")]
+        private float t_CountDown;
+
+        /// <summary>
+        /// 当前是否正处于倒计时中
+        /// </summary>
+        public bool InCountDown => LeftTime > 0f;
+        /// <summary>
+        /// 剩余倒计时时间
+        /// </summary>
+        public float LeftTime => t_CountDown;
+        /// <summary>
+        /// 经过的时间
+        /// </summary>
+        public float PassedTime => countDownFullTime - t_CountDown;
+        /// <summary>
+        /// 剩余倒计时时间
+        /// </summary>
+        public float CountDownProgress => t_CountDown / countDownFullTime;
+
+        /// <summary>
+        /// 开始倒计时
+        /// </summary>
+        /// <param name="time">倒计时时间</param>
+        /// <param name="start">是否立即开始倒计时</param>
+        public void Set(float time, bool start)
+        {
+            countDownFullTime = time;
+            t_CountDown = time;
+            enabled = start;
+            if (start) Begin();
+        }
+        /// <summary>
+        /// 开始倒计时
+        /// </summary>
+        /// <param name="time">倒计时时间</param>
+        public void Set(float time) => Set(time, true);
+
+        /// <summary>
+        /// 开始倒计时
+        /// </summary>
+        public void Begin() => enabled = true;
+
+        /// <summary>
+        /// 暂停倒计时
+        /// </summary>
+        public void Pause() => enabled = false;
+
+        /// <summary>
+        /// 取消暂停，继续倒计时
+        /// </summary>
+        public void UnPause() => enabled = true;
+
+        /// <summary>
+        /// 重设计时器为原最大时间值，并沿用原运行状态倒计时
+        /// </summary>
+        public void Reset() => Reset(InCountDown);
+        /// <summary>
+        /// 重设计时器为原最大时间值
+        /// </summary>
+        /// <param name="start">是否开始倒计时</param>
+        public void Reset(bool start)
+        {
+            t_CountDown = countDownFullTime;
+            enabled = start;
+            UpdateShow();
+        }
+
+        /// <summary>
+        /// 停止计时器
+        /// </summary>
+        /// <param name="updateShow">是否将剩余时间重设为0并更新显示，默认为是</param>
+        public void Stop(bool updateShow = true)
+        {
+            if (updateShow)
+            {
+                t_CountDown = 0f;
+                UpdateShow();
+            }
+            Pause();
+        }
+
+        private void UpdateShow()
+        {
+            if (timeValueText && timeValueText.isActiveAndEnabled)
+                //更新文本显示
+                timeValueText.text = string.Format(contentFormat, t_CountDown, countDownFullTime, LeftTime, CountDownProgress);
+            if (timeValueSlider) timeValueSlider.value = CountDownProgress;
+        }
+
+        private void Update()
+        {
+            if (!(t_CountDown > 0f)) return;
+            //按下任意键时重置倒计时
+            if (resetOnAnyInput)
+            {
+#if ENABLE_INPUT_SYSTEM
+                //使用新输入系统
+                if (UnityEngine.InputSystem.Keyboard.current.anyKey.wasPressedThisFrame)
+#else
+            //使用旧输入系统
+                if (Input.anyKeyDown)
+#endif
+                {
+                    Reset(); 
+                    return;
+                }
+            }
+            //倒计时数据更新，显示更新
+            t_CountDown -= useTimeScale ? Time.deltaTime : Time.unscaledDeltaTime;
+            UpdateShow();
+            //倒计时结束
+            if (t_CountDown > 0) return;
+            onCountDownCompleted?.Invoke(countDownFullTime);
+        }
+    }
+}
---
+++ Runtime/Timer.cs
@@ -0,0 +1,151 @@
+﻿using System;
+
+namespace DreemurrStudio.CountDownTimer
+{
+    /// <summary>
+    /// 封装的计时器类，提供通用的计时功能
+    /// </summary>
+    public class Timer
+    {
+        /// <summary>
+        /// 每秒的计时器刻度数
+        /// </summary>
+        public const int TICK_PERSECOND = 1000;
+
+        /// <summary>
+        /// 剩余时间
+        /// </summary>
+        private float leftTime;
+        /// <summary>
+        /// 完整计时器时间
+        /// </summary>
+        private float fullTime;
+        /// <summary>
+        /// 计时器步长，单位为秒
+        /// </summary>
+        private float step;
+        /// <summary>
+        /// 所使用的时间缩放，不使用时默认为1
+        /// </summary>
+        private float timeScale = 1f;
+        /// <summary>
+        /// 是否处于计时中
+        /// </summary>
+        private bool inCountDown = false;
+        /// <summary>
+        /// 是否已初始化
+        /// </summary>
+        private bool doInit = false;
+
+        /// <summary>
+        /// 计时器开始时事件，输出计时器要运行的时间
+        /// </summary>
+        public event Action<float> OnTimerBegin;
+        /// <summary>
+        /// 计时器结束时事件，输出计时器运行的时间
+        /// </summary>
+        public event Action<float> OnTimerEnd;
+        /// <summary>
+        /// 计时器每次步进时事件，输出当前剩余时间
+        /// </summary>
+        public event Action<float> OnTimerStep;
+
+        /// <summary>
+        /// 实现计时功能的定时器
+        /// </summary>
+        private System.Timers.Timer timer;
+        /// <summary>
+        /// 当前计时器是否在倒计时中
+        /// </summary>
+        public bool InCountDown => inCountDown;
+        /// <summary>
+        /// 获取当前计时器的实际间隔时间
+        /// </summary>
+        public float Interval => step * TICK_PERSECOND / timeScale;
+        /// <summary>
+        /// 获取当前计时器的剩余时间
+        /// </summary>
+        public float LeftTime => leftTime;
+        /// <summary>
+        /// 时间缩放，默认为1
+        /// </summary>
+        public float TimeScale => timeScale;
+
+        /// <summary>
+        /// 设置计时器
+        /// </summary>
+        /// <param name="time">倒计时要到达的完整时间，若要设为</param>
+        /// <param name="step">每次步进</param>
+        /// <param name="timeScale"></param>
+        /// <param name="start"></param>
+        public void Set(float time, float step = 1f, float timeScale = 1f, bool start = true)
+        {
+            leftTime = time;
+            fullTime = time;
+            this.step = step;
+            this.timeScale = timeScale;
+            doInit = false;
+
+            timer = new(step * TICK_PERSECOND / timeScale);
+            timer.AutoReset = true;
+            timer.Elapsed += (sender, e) =>
+            {
+                if (LeftTime <= 0f)
+                {
+                    timer.Stop();
+                    OnTimerEnd?.Invoke(fullTime);
+                    return;
+                }
+                leftTime -= step * timeScale;
+                OnTimerStep?.Invoke(LeftTime);
+            };
+            if (start) Start();
+        }
+
+        /// <summary>
+        /// 重设计时器为原最大时间值
+        /// </summary>
+        /// <param name="start">是否立即开始计时，可设为空，为空时不变更当前运行状态</param>
+        public void Reset(bool? start = null) =>
+            Set(fullTime, step, timeScale, start.HasValue ? start.Value : InCountDown);
+
+        /// <summary>
+        /// 开始计时器
+        /// </summary>
+        public void Start()
+        {
+            Resume();
+            if (!doInit) OnTimerBegin?.Invoke(fullTime);
+            doInit = true;
+        }
+
+        /// <summary>
+        /// 继续计时器
+        /// </summary>
+        public void Resume()
+        {
+            inCountDown = true;
+            timer.Start();
+        }
+
+        /// <summary>
+        /// 暂停计时器
+        /// </summary>
+        public void Pause()
+        {
+            inCountDown = false;
+            timer.Stop();
+        }
+
+        /// <summary>
+        /// 修改计时器的时间缩放
+        /// </summary>
+        /// <param name="scale">要修改的缩放值，不指定时默认为1，即正常值</param>
+        public void SetTimeScale(float scale = 1f)
+        {
+            if (scale < 0f) throw new ArgumentOutOfRangeException(nameof(scale), "时间缩放不能小于0");
+            timer.Interval = step * TICK_PERSECOND / scale;
+            timeScale = scale;
+        }
+    }
+}
